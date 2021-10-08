f803f56ae15a33e8732c163ded0b22b645565472
Largebro <carpowaleks3@gmail.com>
2021-10-08 22:08:52 +0400

    add Github Actions

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
-#!/usr/bin/env node 
-console.log('Welcome to the Brain Games!')
+#!/usr/bin/env node
+import name from '../src/cli';
+
+const answer = `Welcome to the Brain Games!
+May I have your name? ${name}
+Hello, ${name}`;
+console.log(answer);
---
+++ src/cli.js
@@ -0,0 +1,4 @@
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question('Your answer: ');
+export default name;
